269a5292d1cd5bfd16ed28f258bc97b6f8d2cc0b
Ckm54 <mutaic84@gmail.com>
2023-08-05 15:30:07 +0300

    :hammer: create onboarding page setup

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { currentUser } from "@clerk/nextjs";
 
-const page = () => {
+import AccountProfileForm from "@/components/forms/AccountProfileForm";
+import { userInfo } from "os";
+
+const page = async () => {
+  const user = await currentUser();
+
+  const userInfo: any = {};
+
+  const userData: UserInfoType = {
+    id: user?.id || "", // will come from the database
+    objectId: userInfo?._id, // will be from the database
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
   return (
-    <main>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="text-white">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete setting up your profile to use AThreads
+      </p>
+
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfileForm user={userData} btnTitle="Continue" />
+      </section>
     </main>
   );
 };
---
+++ components/forms/AccountProfileForm.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+interface AccountProfileFormProps {
+  user: UserInfoType;
+  btnTitle: string;
+}
+
+const AccountProfileForm = ({ user, btnTitle }: AccountProfileFormProps) => {
+  return <div>AccountProfileForm</div>;
+};
+
+export default AccountProfileForm;
