2e4071935834f3770cc6a79d872ba6af7b4cf06e
Ckm54 <mutaic84@gmail.com>
2023-08-07 22:22:12 +0300

    :hammer: update constants

--- constants/index.ts
+++ constants/index.ts
@@ -16,8 +16,8 @@ export const sidebarLinks = [
   },
   {
     imgURL: "/assets/create.svg",
-    route: "/create-thread",
-    label: "Create Thread",
+    route: "/create",
+    label: "Create Post",
   },
   {
     imgURL: "/assets/community.svg",
